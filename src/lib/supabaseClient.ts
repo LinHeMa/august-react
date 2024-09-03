cfc33b12d584cdface318c372bd010c718c64a20
linhema <linhema0921@gmail.com>
2024-09-03 17:23:21 +0800

    env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,16 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const process = import.meta.env;
-const env = process.MODE;
-console.log(env)
-const supabaseUrl =
-  env === "production"
-    ? import.meta.env.SUPABASE_URL
-    : import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey =
-  env === "production"
-    ? import.meta.env.SUPABASE_ANON_KEY
-    : import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase environment variables");
