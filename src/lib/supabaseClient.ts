207493ba7d0b020327c34ab61da1f01446f1f85d
linhema <linhema0921@gmail.com>
2024-09-03 17:12:59 +0800

    env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const process = import.meta.env;
+const env = process.MODE;
+const supabaseUrl =
+  env === "production"
+    ? import.meta.env.SUPABASE_URL
+    : import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey =
+  env === "production"
+    ? import.meta.env.SUPABASE_URL
+    : import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase environment variables");
