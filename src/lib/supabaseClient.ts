16d19eeecb2325f8ef617f61ffb902d00f2dc4b6
linhema <linhema0921@gmail.com>
2024-09-03 17:16:39 +0800

    env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,13 +2,14 @@ import { createClient } from "@supabase/supabase-js";
 
 const process = import.meta.env;
 const env = process.MODE;
+console.log(env)
 const supabaseUrl =
   env === "production"
     ? import.meta.env.SUPABASE_URL
     : import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey =
   env === "production"
-    ? import.meta.env.SUPABASE_URL
+    ? import.meta.env.SUPABASE_ANON_KEY
     : import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
